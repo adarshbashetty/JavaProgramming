aebab27633d98e35997978020e03ba4c4a5205c9
Adarsh <bashettyadarsh@gmail.com>
2022-07-13 21:11:48 +0530

    Day1

---
+++ Firstprogramme.java
@@ -0,0 +1,13 @@
+class Firstprogramme{
+	public static void main(String[]args){
+	// byte a;
+	// int c=10;
+	// // String a;
+	// System.out.println('a');
+
+		System.out.println('');
+
+
+
+	} 
+}
