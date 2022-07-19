d95d69054d978f774b77793268e553f614c88dce
Adarsh <bashettyadarsh@gmail.com>
2022-07-19 21:14:14 +0530

    DAY3

---
+++ AgeValidation.java
@@ -0,0 +1,12 @@
+class AgeValidation{
+	public static void main (String[] args){
+		int age = 18;
+		if(age>=18){
+			System.out.println("he is eligible to vote");
+		}
+			else { 
+				System.out.println("he is not eligible to vote");
+			}
+		
+	}
+}
