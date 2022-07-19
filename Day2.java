d10b4941588e3d8b02b970a210102eef874058a6
Adarsh <bashettyadarsh@gmail.com>
2022-07-19 20:31:01 +0530

    Day2

---
+++ Day2.java
@@ -0,0 +1,60 @@
+class Day2{
+	public static void main(String[] args){
+
+		// int sum;
+		// int a = 20;
+		// int b = 30;
+		// int c = 10;
+		// int d = 10;
+		// System.out.println(" Multiplication of "+a+" and "+b+" is "+(a*b));
+		
+		// System.out.println(c-d);
+
+
+
+		// if(true){
+		// 	System.out.println("Inside if");
+		// }
+
+
+
+		// if(true){
+		// 	System.out.println("Inside if");
+		// }
+		// else{
+		// 	System.out.println("Inside else");
+		// }
+
+		// if(false){
+		// 	System.out.println("Inside if");
+		// }
+		// else{
+		// 	System.out.println("Inside else");
+		// }
+
+
+		// int a;
+		// if('a'==a){
+		// 	System.out.println("Inside if");
+		// }
+		// else{
+		// 	System.out.println("Inside else");
+		// }
+		//op=comiple error
+
+		// int a=10;
+		// if('a'==a){
+		// 	System.out.println("Inside if");
+		// }
+		// else{
+		// 	System.out.println("Inside else");
+		// }
+
+		// if(10<=10){
+
+		// }
+		// else{
+
+		// }
+}
+}
--- Firstprogramme.java
+++ Firstprogramme.java
@@ -1,11 +1,80 @@
 class Firstprogramme{
+
 	public static void main(String[]args){
 	// byte a;
 	// int c=10;
 	// // String a;
-	// System.out.println('a');
+	// // char a='a';
+	// System.out.println(a+10);
 
-		System.out.println('');
+	// 	// System.out.println(10+2.0);
+
+
+
+		// String x="hello";
+		// System.out.println(x);
+		//op=hello
+
+		// String x="hello";
+		// System.out.println('x');
+		//op=x
+
+
+		// String x=;
+		// System.out.println(x);
+		//compile error '='
+
+		// String x;
+		// System.out.println(x);
+		//comiple error not initialize
+
+		// String x=null;
+		// System.out.println(x);
+
+		//redecalration(not allowed)
+		// int a;
+		// int a;
+
+		//reinitialization(allowed)
+		// int a;
+		// a=10;
+		// a=20;
+		// System.out.println(a);
+		//op=20
+
+		// int a=10;
+		// int b=20;
+		// double x=a+b;
+		// System.out.println(x);
+		//type conversion error
+
+		// float a=10;
+		// float b=20;
+		// long x=a+b;
+		// System.out.println(x);
+		//type conversion error
+
+		// int c=a+b;
+
+		//operators
+		//single operands required
+		// & bitwise AND
+		// | bitwise OR
+
+		//double operands required
+		// = assign
+		// - substraction
+		// == equals
+		// != not equal
+		// % percentile (10%3=1)
+		// / devide by
+		// + addition 
+		// * multiplication
+		//  && AND
+		//  || OR
+
+		//triple operands
+		// System.out.println(10==20?"wrong":"Correct");
 
 
 
